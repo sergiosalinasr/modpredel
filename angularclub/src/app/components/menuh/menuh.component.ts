2ff54d859854ba413e739d62ffe81b0e4bdfa2f8
sergiosalinasr <sergio.salinasr@gmail.com>
2024-06-06 21:40:36 -0400

    master: Haciendo ajustes para componente menu- funcionando. Sgte: conectar componente login con menu

---
+++ angularclub/src/app/components/menuh/menuh.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menuh',
+  templateUrl: './menuh.component.html',
+  styleUrl: './menuh.component.css'
+})
+export class MenuhComponent {
+
+}
