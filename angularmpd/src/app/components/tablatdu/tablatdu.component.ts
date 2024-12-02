fb9e4ed69377d5912bf49fc2e025090458af2024
sergiosalinasr <sergio.salinasr@gmail.com>
2024-12-02 11:39:29 -0300

    TablaTdu CRUD - Completado

--- angularmpd/src/app/components/tablatdu/tablatdu.component.ts
+++ angularmpd/src/app/components/tablatdu/tablatdu.component.ts
@@ -24,17 +24,74 @@ export class TablatduComponent {
   addOrEdit(){
 
     // Si no hay emppleado seleccionado...
-    if (this.selectedTablatdu.id == 0){
-      
+    if (this.selectedTablatdu.id === 0){
+      confirm("addOrEdit: this.selectedTablatdu.id == 0 CREATE")
+      this.tablatduService.postTablatdu(this.selectedTablatdu).subscribe( data =>{
+        let dataResponse:any=data;
+        if (dataResponse.id == this.selectedTablatdu.id){
+          //this.alertas.showSuccess('Datos modificados', 'Hecho');
+          this.tablatduArray.push(this.selectedTablatdu)
+        }else{
+          // this.alertas.showError('Datos NO modificados','Error')
+        }
+      })
+
+      this.selectedTablatdu = new Tablatdu(0, "", "");
+    }  else {
+      //confirm("addOrEdit: this.selectedTablatdu.id == 0 UPDATE")
+      this.tablatduService.putTablatdu(this.selectedTablatdu).subscribe( data =>{
+        let dataResponse:any=data;
+        if (dataResponse.id == this.selectedTablatdu.id){
+          //this.alertas.showSuccess('Datos modificados', 'Hecho');
+        }else{
+          // this.alertas.showError('Datos NO modificados','Error')
+        }
+      })
+
+      this.selectedTablatdu = new Tablatdu(0, "", "");
     }
     
     this.selectedTablatdu = new Tablatdu(0, "", "");
   }
 
   openForEdit(tablatdu: Tablatdu){
-    if ( this.selectedTablatdu.id == 0){
+    //this.selectedTablatdu = tablatdu;
+    
+    if ( this.selectedTablatdu.id === 0){
+      //confirm("openForEdit: this.selectedTablatdu.id == 0 TRUE")
       this.selectedTablatdu = tablatdu;
-    } else {
+    } 
+    else {
+      /*
+      this.tablatduService.putTablatdu(this.selectedTablatdu).subscribe( data =>{
+        let dataResponse:any=data;
+        if (dataResponse.id == this.selectedTablatdu.id){
+          //this.alertas.showSuccess('Datos modificados', 'Hecho');
+        }else{
+          // this.alertas.showError('Datos NO modificados','Error')
+        }
+      })
+      */
+      this.selectedTablatdu = new Tablatdu(0, "", "");
+    }
+    
+  }
+
+  delete(){
+    if (confirm("Are yu sure you want to deleted it?")){
+      this.tablatduService.deleteTablatdu(this.selectedTablatdu).subscribe( data =>{
+        let dataResponse:any=data;
+        if (dataResponse.id == this.selectedTablatdu.id){
+          //this.alertas.showSuccess('Datos modificados', 'Hecho');
+          this.tablatduArray = this.tablatduArray.filter(item => item.id !== this.selectedTablatdu.id);
+
+          //this.tablatduArray = this.tablatduArray.filter( x => x != this.selectedTablatdu);
+          //this.tablatduArray.push(this.selectedTablatdu)
+        }else{
+          // this.alertas.showError('Datos NO modificados','Error')
+        }
+      })
+      this.tablatduArray = this.tablatduArray.filter(item => item.id !== this.selectedTablatdu.id);
       this.selectedTablatdu = new Tablatdu(0, "", "");
     }
     
--- angularmpd/src/app/services/tablatdu.service.ts
+++ angularmpd/src/app/services/tablatdu.service.ts
@@ -17,4 +17,33 @@ export class TablatduService {
     return this.http.get<Tablatdu[]>(direccion);
   }
 
+  putTablatdu(form:Tablatdu):Observable<any>{
+    // Método PUT para actualizar una TDU
+    //updateTdu(id: number, body: { nombreCorto: string, descripcionLarga: string }): Observable<any> {
+    const url = this.url_mpd + "/tdu/" + form.id; // URL completa con el ID
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = { nombreCorto: form.nombreCorto, descripcionLarga: form.descripcionLarga };
+    return this.http.put(url, body, { headers });
+    //}
+  }
+
+  postTablatdu(form:Tablatdu):Observable<any>{
+
+    const url = this.url_mpd + "/tdu/";
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = { nombreCorto: form.nombreCorto, descripcionLarga: form.descripcionLarga };
+    return this.http.post(url, body, { headers });
+
+  }
+
+  deleteTablatdu(form:Tablatdu):Observable<any>{
+    // Método PUT para actualizar una TDU
+    //updateTdu(id: number, body: { nombreCorto: string, descripcionLarga: string }): Observable<any> {
+    const url = this.url_mpd + "/tdu/" + form.id; // URL completa con el ID
+    //const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    //const body = { nombreCorto: form.nombreCorto, descripcionLarga: form.descripcionLarga };
+    return this.http.delete(url);
+    //}
+  }
+
 }
