e8cf5cafb32262fb541fc7c3cf1c68c8f1112c81
sergiosalinasr <sergio.salinasr@gmail.com>
2024-12-15 22:06:50 -0300

    Tabla ley try prueba API listas. Cont: Integrar pruebas Newman

--- nodempd/src/api/controllers/leyController.js
+++ nodempd/src/api/controllers/leyController.js
@@ -8,7 +8,6 @@ exports.getley = async (req, res) => {
   const response = await pool.query("SELECT id, nombre, descripcion, fechapublicacion, pais FROM " + schema + ".ley ORDER BY id ASC");
   res.status(200).json(response.rows);
 } catch (error) {
-  console.log("error: " + error.message)
   return res.status(400).json({ error: error.message });
 }
 
@@ -18,9 +17,8 @@ exports.getleyById = async (req, res) => {
   try {
     const id = parseInt(req.params.id);
     const response = await pool.query("SELECT id, nombre, descripcion, fechapublicacion, pais FROM " + schema + ".ley WHERE id = $1", [id]);
-    res.status(400).json(response.rows);
+    res.status(200).json(response.rows);
   } catch (error) {
-    console.log("error: " + error.message)
     return res.status(404).json({ error: error.message });
   }
 };
@@ -39,30 +37,36 @@ exports.createley = async (req, res) => {
 
     res.status(201).json(rows[0]);
   } catch (error) {
-    console.log("error: " + error.message)
     return res.status(500).json({ error: error.message });
   }
 };
 
 exports.updateley = async (req, res) => {
-  const id = parseInt(req.params.id);
-  const { nombre, descripcion, fechapublicacion, pais } = req.body;
-  const fechapublicacionFormateada = new Date(fechapublicacion);
-  const { rows } = await pool.query(
-    "UPDATE " + schema + ".ley SET nombre = $2, descripcion = $3, fechapublicacion = $4, pais = $5 WHERE id = $1 RETURNING *",
-    [id, nombre, descripcion, fechapublicacionFormateada, pais]
-  );
-  return res.status(200).json(rows[0]);
+  try {
+    const id = parseInt(req.params.id);
+    const { nombre, descripcion, fechapublicacion, pais } = req.body;
+    const fechapublicacionFormateada = new Date(fechapublicacion);
+    const { rows } = await pool.query(
+      "UPDATE " + schema + ".ley SET nombre = $2, descripcion = $3, fechapublicacion = $4, pais = $5 WHERE id = $1 RETURNING *",
+      [id, nombre, descripcion, fechapublicacionFormateada, pais]
+    );
+    return res.status(200).json(rows[0]);
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
 };
 
 exports.deleteley = async (req, res) => {
-  const id = parseInt(req.params.id);
-  const { rowCount } = await pool.query("DELETE FROM " + schema + ".ley where id = $1", [
-    id,
-  ]);
-
-  if (rowCount === 0) {
-    return res.status(404).json({ message: "ley not found" });
+  try {
+    const id = parseInt(req.params.id);
+    const { rowCount } = await pool.query("DELETE FROM " + schema + ".ley where id = $1", [
+      id,
+    ]);
+    if (rowCount === 0) {
+      return res.status(404).json({ message: "ley not found" });
+    }
+    return res.status(200).json({ eliminado: "ELIMINADO" });
+  } catch (error) {
+    return res.status(404).json({ error: error.message });
   }
-  return res.sendStatus(204);
 };
