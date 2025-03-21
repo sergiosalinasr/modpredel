e13f612b55f7495449f8c16c930c3ea8d67b1572
sergiosalinasr <sergio.salinasr@gmail.com>
2025-03-21 18:05:33 -0300

    Postman: pruebas con Newman y token ok. Sgte:

--- nodempd/src/api/routes/cduRoutes.js
+++ nodempd/src/api/routes/cduRoutes.js
@@ -6,12 +6,12 @@ const cduController = require('../controllers/cduController');
 const keycloak = require('../../config/keycloak');
 
 console.log("En delitoRoutes")
-router.post("/", cduController.createcdu);
-router.get("/", cduController.getcdu);
-router.get("/getcducampos", cduController.getcducampos); // OJO: los sin ID ANTES de los con :id ¿?
-router.get('/tdu/:id_tdu', cduController.getCDUsByTDU);
-router.get("/:id", cduController.getcduById);
-router.put("/:id", cduController.updatecdu);
-router.delete("/:id", cduController.deletecdu);
+router.post("/", keycloak.protect(), cduController.createcdu);
+router.get("/", keycloak.protect(), cduController.getcdu);
+router.get("/getcducampos", keycloak.protect(), cduController.getcducampos); // OJO: los sin ID ANTES de los con :id ¿?
+router.get('/tdu/:id_tdu', keycloak.protect(), cduController.getCDUsByTDU);
+router.get("/:id", keycloak.protect(), cduController.getcduById);
+router.put("/:id", keycloak.protect(), cduController.updatecdu);
+router.delete("/:id", keycloak.protect(), cduController.deletecdu);
 
 module.exports = router;
--- nodempd/src/api/routes/tduRoutes.js
+++ nodempd/src/api/routes/tduRoutes.js
@@ -6,11 +6,11 @@ const tduController = require('../controllers/tduController');
 const keycloak = require('../../config/keycloak');
 
 console.log("En delitoRoutes")
-router.post("/", tduController.createtdu);
-router.get("/", tduController.gettdu);
-router.get("/gettducampos", tduController.gettducampos); // OJO: los sin ID ANTES de los con :id ¿?
-router.get("/:id", tduController.gettduById);
-router.put("/:id", tduController.updatetdu);
-router.delete("/:id", tduController.deletetdu);
+router.post("/", keycloak.protect(), tduController.createtdu);
+router.get("/", keycloak.protect(), tduController.gettdu);
+router.get("/gettducampos", keycloak.protect(), tduController.gettducampos); // OJO: los sin ID ANTES de los con :id ¿?
+router.get("/:id", keycloak.protect(), tduController.gettduById);
+router.put("/:id", keycloak.protect(), tduController.updatetdu);
+router.delete("/:id", keycloak.protect(), tduController.deletetdu);
 
 module.exports = router;
