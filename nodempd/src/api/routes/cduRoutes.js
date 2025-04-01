e2e9944f203e5c2944fce0a304354d8596c18353
sergiosalinasr <sergio.salinasr@gmail.com>
2025-04-01 13:04:17 -0300

    Node: aplicar keycloak.protect() a todas las rutas  ok; Sgte.: Postman: agregar nuevas rutas en las pruebas

--- nodempd/src/api/routes/cduRoutes.js
+++ nodempd/src/api/routes/cduRoutes.js
@@ -8,8 +8,8 @@ const keycloak = require('../../config/keycloak');
 console.log("En delitoRoutes")
 router.post("/", keycloak.protect(), cduController.createcdu);
 router.get("/", keycloak.protect(), cduController.getcdu);
-router.get("/getcducampos", cduController.getcducampos); // OJO: los sin ID ANTES de los con :id ¿?
-router.get("/getcducampos/:id_tdu", cduController.getcducamposid_tdu);
+router.get("/getcducampos", keycloak.protect(), cduController.getcducampos); // OJO: los sin ID ANTES de los con :id ¿?
+router.get("/getcducampos/:id_tdu", keycloak.protect(), cduController.getcducamposid_tdu);
 router.get('/tdu/:id_tdu', keycloak.protect(), cduController.getCDUsByTDU);
 router.get("/:id", keycloak.protect(), cduController.getcduById);
 router.put("/:id", keycloak.protect(), cduController.updatecdu);
